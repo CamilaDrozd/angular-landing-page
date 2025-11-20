9604090a209ee75c0748c8093cb9083ad401386a
Camila <camilad.palma@gmail.com>
2025-11-20 09:46:12 -0300

    feat: criação do formulario

---
+++ src/app/components/news-form/news-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
+
+@Component({
+  selector: 'news-form',
+  standalone: true,
+  imports: [BtnPrimaryComponent],
+  templateUrl: './news-form.component.html',
+  styleUrl: './news-form.component.scss'
+})
+export class NewsFormComponent {
+
+}
