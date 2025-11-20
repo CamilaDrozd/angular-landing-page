25655a2d14f7adfa1f7b92cce05a0566be4893f6
Camila <camilad.palma@gmail.com>
2025-11-20 15:20:35 -0300

    feat: ações para inscrição do formulário

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
+import { FormControl, FormControlName, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
+import { emit } from 'process';
 
 @Component({
   selector: 'news-form',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers: [
+    NewsletterService
+  ],
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.scss'
 })
 export class NewsFormComponent {
 
+  newsletterForm!: FormGroup;
+  loading = signal(false);
+
+  constructor(private service: NewsletterService){
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  onSubmit(){
+    this.loading.set(true);
+    if(this.newsletterForm.valid){
+      this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email).subscribe({
+        next: () => {
+          this.newsletterForm.reset();
+          this.loading.set(false);
+        },
+        error: () => this.loading.set(false)
+      })
+    }
+  }
 }
